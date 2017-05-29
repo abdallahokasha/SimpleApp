428e568f507588fbc77f7d2217ce95d0f1e8356c
okasha93 <abdookasha56@gmail.com>
2017-05-29 15:55:44 +0200

    four functions done & routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  resources :employees
-  resources :companies
+  resources :companies do
+  	get :assign, on: :member
+  end
+  
+  resources :employees do
+  	get :apply, on: :member
+  end  
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

2bf598a9bf2dc514c291746373612e7c99ab6a30
okasha93 <abdookasha56@gmail.com>
2017-06-18 15:21:24 +0200

    Sign-in is done using angular-devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-  # before_action :authenticate_user!
+  #protect_from_forgery with: :exception
+  respond_to :html, :json
+  protect_from_forgery with: :exception, unless: -> { request.format.json? }
+  before_action :authenticate_user!
 end
--- my_views/sign_in.js
+++ my_views/sign_in.js
@@ -1,3 +1,30 @@
+( function() {
+  'use strict';
+
+  angular.module('signIn', ['Devise'])
+   .config(function(AuthProvider) {
+        AuthProvider.loginPath('http://localhost:3000/users/sign_in.json');
+        AuthProvider.loginMethod('POST');
+        AuthProvider.resourceName('user');
+    })
+  .controller('signInCtrl', ['Auth', '$scope', '$location',
+    function(Auth, $scope, $location) {
+      this.credentials = { email: '', password: '' };
+
+      this.signIn = function() {
+        // Code to use 'angular-devise' component
+        Auth.login(this.credentials).then(function(user) {
+          //$location.path("/");
+          alert('Successfully signed in user!')
+        }, function(error) {
+          console.info('Error in authenticating user!');
+          alert('Error in signing in user!');
+        });
+      }
+    }
+  ]);
+})();
+
 // var app = angular.module('myApp', []);
 
 // app.controller('signinController', function($scope, $http) {
@@ -20,42 +47,42 @@
 // });
    
 
-var myApp = angular.module('myApp', ['Devise'])
+// var myApp = angular.module('myApp', ['Devise'])
 
-myApp.config(function(AuthProvider, AuthInterceptProvider) {
-        AuthProvider.loginPath('/users/sign_in.json');
-        AuthProvider.loginMethod('POST');
-        AuthProvider.resourceName('user');
+// myApp.config(function(AuthProvider, AuthInterceptProvider) {
+//         AuthProvider.loginPath('/users/sign_in.json');
+//         AuthProvider.loginMethod('POST');
+//         AuthProvider.resourceName('user');
 
-        AuthInterceptProvider.interceptAuth(true);
-    });
+//         AuthInterceptProvider.interceptAuth(true);
+//     });
 
-    myApp.controller('signinController', function($scope, Auth) {
-        var credentials = {
-            email: 'abc@xyz.com',
-            password: '123456'
-        };
-        // var config = {
-        //     headers: {
-        //         'X-HTTP-Method-Override': 'POST'
-        //     }
-        // };
+//     myApp.controller('signinController', function($scope, Auth) {
+//         var credentials = {
+//             email: 'abc@xyz.com',
+//             password: '123456'
+//         };
+//         // var config = {
+//         //     headers: {
+//         //         'X-HTTP-Method-Override': 'POST'
+//         //     }
+//         // };
 
-        Auth.login(credentials).then(function(user) {
-            console.log(user); // => {id: 1, ect: '...'}
-        }, 
+//         Auth.login(credentials).then(function(user) {
+//             console.log(user); // => {id: 1, ect: '...'}
+//         }, 
 
-        function(error) {
-            // Authentication failed...
-        });
+//         function(error) {
+//             // Authentication failed...
+//         });
 
-        $scope.$on('devise:login', function(event, currentUser) {
-            // after a login, a hard refresh, a new tab
-        });
+//         $scope.$on('devise:login', function(event, currentUser) {
+//             // after a login, a hard refresh, a new tab
+//         });
 
-        $scope.$on('devise:new-session', function(event, currentUser) {
-            // user logged in by Auth.login({...})
-        });
-    });
+//         $scope.$on('devise:new-session', function(event, currentUser) {
+//             // user logged in by Auth.login({...})
+//         });
+//     });
 
     
